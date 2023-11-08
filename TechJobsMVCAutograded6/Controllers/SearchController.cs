b4134c2bcf4e29d4e6e99fd91156ceb2f2bebca5
haylied <hayliedarland@gmail.com>
2023-11-07 20:38:35 -0600

    commit -m "finished task four"

--- TechJobsMVCAutograded6/Controllers/SearchController.cs
+++ TechJobsMVCAutograded6/Controllers/SearchController.cs
@@ -15,7 +15,7 @@ public class SearchController : Controller
     public IActionResult Index()
     {
         ViewBag.columns = ListController.ColumnChoices;
-        return View(jobs);
+        return View();
     }
 
     // TODO #3 - Create an action method to process a search request and render the updated search views.
