c3c968112ef1e65b7cf0fae1ef5b0a82e4c26122
haylied <hayliedarland@gmail.com>
2023-11-07 19:50:37 -0600

    commit

--- TechJobsMVCAutograded6/Controllers/SearchController.cs
+++ TechJobsMVCAutograded6/Controllers/SearchController.cs
@@ -10,13 +10,27 @@ namespace TechJobsMVCAutograded6.Controllers;
 
 public class SearchController : Controller
 {
+    List<Job> jobs = new List<Job>();
+
     // GET: /<controller>/
     public IActionResult Index()
     {
         ViewBag.columns = ListController.ColumnChoices;
-        return View();
+        return View(jobs);
     }
 
     // TODO #3 - Create an action method to process a search request and render the updated search views.
+    public IActionResult Results(string searchType, string searchTerm)
+    {
+        if (searchTerm == "all" || searchTerm == "")
+        {
+            jobs = JobData.FindAll();
+        } else
+        {
+            jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+        }
+        return View();
+
+    }
 }
 
